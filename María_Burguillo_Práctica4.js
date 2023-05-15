2e0c09f4b5ec51b559e1023a3467a7a6d02d58dc
maalbupe <maalbupe@gmail.com>
2023-05-15 11:43:05 +0200

    Modificaciones

--- María_Burguillo_Práctica4.js
+++ María_Burguillo_Práctica4.js
@@ -1,52 +1,107 @@
-const array = [];
-const longitudArray = 16;
+//PROMISE
 
-while (array.length < longitudArray) {
-  const randomNum = Math.floor(Math.random() * 16) + 1;
-  //math.floor es para redondear porque sino te da numeros con muchos decimales
+const cartas = document.getElementById("card");
 
-  if (!array.includes(randomNum)) {
-    array.push(randomNum);
+let coincidencias = 0;
+let cartauno, cartados;
+let descactivar = false;
 
-    //Se verifica si el número randomNum no está presente en el array utilizando el método includes()
-    //si el numero aleatorio no está en el array la condicion se evalua cono true y lo mete dentro del array
+//se ejecuta cuando se hace click en una carta del juego
+function flipCard({ target: clickedCard }) {
+  if (cartauno !== clickedCard && !descactivar) {
+    /*
+      Esta condición verifica si la carta seleccionada no es igual a la cartauno
+      y si las cartas no están desactivadas. (Esto garantiza que solo 
+      se pueda seleccionar una nueva carta si no se ha desactivado el mazo
+       y si no se está seleccionando la misma carta nuevamente)
+      */
+    clickedCard.classList.add("flip");
+    //Agrega la clase CSS "flip" a la carta seleccionada. tiene estilos asociados para voltear visualmente la carta.
+
+    if (!cartauno) {
+      return (cartauno = clickedCard);
+    }
+    /*
+        Verifica si cartauno es null o sin definir. Si es así, 
+        asigna la clickedCard a cartauno y luego devulve la función. 
+        Esto significa que la carta seleccionada se guarda como la primera carta (cartauno) 
+        y se espera a que se seleccione otra carta.
+        */
+    cartados = clickedCard;
+    descactivar = true;
+    let cartaunoImg = cartauno.getElementById("vistaFrontal").src,
+      cartadosImg = cartados.getElementById("vistaTrasera").src;
+      coincidenciasdelascartas(cartaunoImg, cartadosImg);
+    /*
+    Llama a la función matchCards() pasando las rutas de las imágenes de las dos cartas seleccionadas. 
+    Esta función se encarga de verificar si hay una coincidencia entre las cartas.
+    */
   }
 }
 
-//Creo un objeto imagenes que asocia cada número aleatorio con una imagen.
-//Utilizo un bucle for para recorrer todos los divs y asignar la imagen correspondiente al fondo del div
-
-const imagenes = {
-  1: "arandanos.jpg",
-  2: "cocosmejorados.jpg",
-  3: "maguitos.jpg",
-  4: "manzanas.jpg",
-  5: "melocoton.jpg",
-  6: "peritas.jpg",
-  7: "platanos.jpg",
-  8: "uvas.jpg",
-};
-/*
-para que este código funcione correctamente, tengo 16 divs dentro del contenedor 1, 
-sino obtengo un error cuando intento seleccionar los divs (ya que la longitud del array es 16)
-*/
-const divs = document.getElementById("1").children;
-/*  [.children]para obtener una lista de todos los elementos hijos dentro del contenedor con el ID "1".
- Esto permitirá acceder a los <div> individuales dentro del contenedor.*/
-for (let i = 0; i < divs.length; i++) {
-  const imagen = imagenes[array[i]];
-  divs[i].style.backgroundImage = `url(${imagen})`;
+function coincidenciasdelascartas(img1, img2) {
+  if (img1 === img2) {
+    coincidencias++;
+    if (coincidencias == 8) {
+      setTimeout(() => {
+        return shuffleCard();
+        //Después de un segundo , si se han emparejado todas las cartas llama a ese método para mezclarlas
+      }, 1000);
+    }
+    cartauno.removeEventListener("click", flipCard);
+    cartados.removeEventListener("click", flipCard);
+    cartauno = cartados = "";
+    // esas tres lineas , quitan los eventos para que no se activen de nuevo y ponen las cadenas vacias
+    return (descactivar = false);
+  }
+//se ponen de nuevo las cosas , para que se puedan volver a seleccionar y que tengas efectos
+  setTimeout(() => {
+    cartauno.classList.add("shake");
+    cartados.classList.add("shake");
+  }, 400);
+  setTimeout(() => {
+    cartauno.classList.remove("shake", "flip");
+    cartados.classList.remove("shake", "flip");
+    cartauno = cartados = "";
+    descactivar = false;
+  }, 1200);
 }
 
+function shuffleCard() {
+  coincidencias = 0;
+  descactivar = false;
+  carcartaunodOne = cartados = "";
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+  arr.sort(() => (Math.random() > 0.5 ? 1 : -1));
+  /*
+  mezcla aleatoriamente los elementos del array . 
+  gracias a función de comparación que devuelve un número positivo o negativo de manera aleatoria. 
+  por ello los elementos del array tienen en un orden aleatorio.
+  */
+  cartas.forEach((card, i) => {
+    card.classList.remove("flip");
+    let imgTag = card.getElementById("vistaTrasera");
+    imgTag.src = `images/img-${arr[i]}.png`;
+    card.addEventListener("click", flipCard);
+  });
+}
+
+shuffleCard();
+
+cartas.forEach((card) => {
+  card.addEventListener("click", flipCard);
+});
 
 //intento del cronometro
-const cronometro = document.getElementById('timer');
+const cronometro = document.getElementById("timer");
 let cronometroDuracion = 0;
 
 // Función para actualizar el temporizador
 function updateTimer() {
-  const minutos = Math.floor(cronometroDuracion / 60).toString().padStart(2, '0');
-  const segundos = (cronometroDuracion % 60).toString().padStart(2, '0');
+  const minutos = Math.floor(cronometroDuracion / 60)
+    .toString()
+    .padStart(2, "0");
+  const segundos = (cronometroDuracion % 60).toString().padStart(2, "0");
   cronometro.textContent = `${minutos}:${segundos}`;
   cronometroDuracion++;
 }
@@ -56,14 +111,11 @@ const iniciocrono = setInterval(updateTimer, 1000);
 
 // fin intento del cronometro
 
-
-
 let parejasEncontradas = 0;
 const numeroTotalParejas = 8;
 let timerDuration = 0;
 
 //  encontrar parejas de cartas
-
 
 // Cuando el jugador encuentra una pareja de cartas
 parejasEncontradas++;
@@ -74,5 +126,3 @@ if (parejasEncontradas === numeroTotalParejas) {
   // mostrar un mensaje de victoria o algo así
   clearInterval(intervalId); // Detener el temporizador
 }
-
-
