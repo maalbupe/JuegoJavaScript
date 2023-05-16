ac1ed1567cef0776b6558d717fb30a8cd7267cca
maalbupe <maalbupe@gmail.com>
2023-05-16 19:00:25 +0200

    mejoras2

--- María_Burguillo_Práctica4.js
+++ María_Burguillo_Práctica4.js
@@ -1,39 +1,58 @@
 //PROMISE
 
-const cartas = document.getElementById("card");
+const cartas = document.querySelectorAll(".card");
+
+/*
+He puesto el querySelectorAll porque de otras maneras me daba error 
+*/
 
 let coincidencias = 0;
 let cartauno, cartados;
 let descactivar = false;
 
 //se ejecuta cuando se hace click en una carta del juego
-function flipCard({ target: clickedCard }) {
-  if (cartauno !== clickedCard && !descactivar) {
+function voltearLaCarta({ currentTarget: cartaclicada }) {
+  /*El evento target representa el elemento real en el que se hizo clic,
+   mientras que el evento currentTarget representa el elemento en el que se adjuntó el controlador del evento 
+  */
+  if (cartauno !== cartaclicada && !descactivar) {
     /*
       Esta condición verifica si la carta seleccionada no es igual a la cartauno
       y si las cartas no están desactivadas. (Esto garantiza que solo 
       se pueda seleccionar una nueva carta si no se ha desactivado el mazo
        y si no se está seleccionando la misma carta nuevamente)
       */
-    clickedCard.classList.add("flip");
+       cartaclicada.classList.add("flip");
     //Agrega la clase CSS "flip" a la carta seleccionada. tiene estilos asociados para voltear visualmente la carta.
-
+//al ejecutar cartaclicada.classList.add("flip") se aplica una transformación visual a cartaclicada para mostrar su cara frontal. Esto se realiza añadiendo la clase CSS "flip" al elemento.
     if (!cartauno) {
-      return (cartauno = clickedCard);
+      return (cartauno = cartaclicada);
     }
     /*
         Verifica si cartauno es null o sin definir. Si es así, 
-        asigna la clickedCard a cartauno y luego devulve la función. 
+        asigna la cartaclicada a cartauno y luego devulve la función. 
         Esto significa que la carta seleccionada se guarda como la primera carta (cartauno) 
         y se espera a que se seleccione otra carta.
         */
-    cartados = clickedCard;
+    cartados = cartaclicada;
     descactivar = true;
-    let cartaunoImg = cartauno.getElementById("vistaFrontal").src,
-      cartadosImg = cartados.getElementById("vistaTrasera").src;
-      coincidenciasdelascartas(cartaunoImg, cartadosImg);
+
+    let cartaunoImg = null;
+    let cartadosImg = null;
+
+    if (cartauno.querySelector(".vistaFrontal")) {
+      cartaunoImg = cartauno.querySelector(".vistaFrontal").src;
+    }
+
+    if (cartados.querySelector(".vistaTrasera")) {
+      cartadosImg = cartados.querySelector(".vistaTrasera").src;
+    }
+
+    //esto creo que lo hice proque sino me daba error diciendome que estba en null entonces me aseguro de que no esten en null
+
+    coincidenciasdelascartas(cartaunoImg, cartadosImg);
     /*
-    Llama a la función matchCards() pasando las rutas de las imágenes de las dos cartas seleccionadas. 
+    Llama a la función coincidenciasdelascartas() pasando las rutas de las imágenes de las dos cartas seleccionadas. 
     Esta función se encarga de verificar si hay una coincidencia entre las cartas.
     */
   }
@@ -44,17 +63,17 @@ function coincidenciasdelascartas(img1, img2) {
     coincidencias++;
     if (coincidencias == 8) {
       setTimeout(() => {
-        return shuffleCard();
+        return cambiarcartas();
         //Después de un segundo , si se han emparejado todas las cartas llama a ese método para mezclarlas
       }, 1000);
     }
-    cartauno.removeEventListener("click", flipCard);
-    cartados.removeEventListener("click", flipCard);
+    cartauno.removeEventListener("click", voltearLaCarta);
+    cartados.removeEventListener("click", voltearLaCarta);
     cartauno = cartados = "";
     // esas tres lineas , quitan los eventos para que no se activen de nuevo y ponen las cadenas vacias
     return (descactivar = false);
   }
-//se ponen de nuevo las cosas , para que se puedan volver a seleccionar y que tengas efectos
+  //se ponen de nuevo las cosas , para que se puedan volver a seleccionar y que tengan efectos
   setTimeout(() => {
     cartauno.classList.add("shake");
     cartados.classList.add("shake");
@@ -67,10 +86,11 @@ function coincidenciasdelascartas(img1, img2) {
   }, 1200);
 }
 
-function shuffleCard() {
+cambiarcartas();
+function cambiarcartas() {
   coincidencias = 0;
   descactivar = false;
-  carcartaunodOne = cartados = "";
+  cartauno = cartados = "";
   let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
   arr.sort(() => (Math.random() > 0.5 ? 1 : -1));
   /*
@@ -80,17 +100,12 @@ function shuffleCard() {
   */
   cartas.forEach((card, i) => {
     card.classList.remove("flip");
-    let imgTag = card.getElementById("vistaTrasera");
+    let imgTag = card.querySelector(".vistaTrasera");
     imgTag.src = `images/img-${arr[i]}.png`;
-    card.addEventListener("click", flipCard);
+    card.addEventListener("click", voltearLaCarta);
   });
+  //este código se encarga de poner las cartas igual que antes, asignar imágenes a las cartas y agregar un controlador de eventos para voltear las cartas al hacer clic en ellas.
 }
-
-shuffleCard();
-
-cartas.forEach((card) => {
-  card.addEventListener("click", flipCard);
-});
 
 //intento del cronometro
 const cronometro = document.getElementById("timer");
